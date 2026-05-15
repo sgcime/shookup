302a0fd86b181b4fa951c2041bc8157a1c730d7a
sgcime <108377162+sgcime@users.noreply.github.com>
2026-05-15 19:22:37 +0900

    Add files via upload

--- seeup-clone/src/components/contact_Navigation.tsx
+++ seeup-clone/src/components/contact_Navigation.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { useState } from "react";
 import { Menu, X } from "lucide-react";
+import Image from 'next/image';
 
 const navItems = [
   { name: "소개", href: "#", active: false },
--- seeup-clone/src/components/projectHeader.tsx
+++ seeup-clone/src/components/projectHeader.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { useState } from "react";
+import Image from 'next/image';
 
 export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
